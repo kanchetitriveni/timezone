d9a55470f80ff262df7c2a8968a07a5f23bf3858
kanchetitriveni <135946009+kanchetitriveni@users.noreply.github.com>
2023-08-10 13:22:45 +0530

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,13 @@
+function Product(props) {
+    return (
+      <div>
+        <img src={props.img} alt="products" />
+        <h4>{props.name}</h4>
+        <p>{props.description}</p>
+        <h4>{props.price}</h4>
+      </div>
+    );
+}
+
+export default Product
+
